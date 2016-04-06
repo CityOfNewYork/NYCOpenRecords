eebb2707610303deaa0c0dc972fc7d26c42ff896
aot221 <aot221@nyu.edu>
2016-04-06 17:57:43 -0400

    added https back to run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from public_records_portal.prflask import app
 from os.path import isdir
 
 if isdir('.certs'):
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, ssl_context=('.certs/openrecords.crt', '.certs/openrecords.key'))
 else:
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, ssl_context='adhoc')
