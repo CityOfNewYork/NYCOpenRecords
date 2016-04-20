2a37f34753f79274e105916b8348e97a382264f9
estevez90 <hestevez@records.nyc.gov>
2016-04-20 14:32:29 -0400

    add utils.py

---
+++ public_records_portal/utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+....public_records_portal.utils
+
+....Implements commonly used functions for handling data
+"""
+
+def strip_html(html_str):
+    """
+    a wrapper for bleach.clean() that strips ALL tags from the input
+    :param html_str: string that needs to be stripped
+    :return: a bleached string
+    """
+    tags = []
+    attr = {}
+    styles = []
+    strip = True
+
+    return bleach.clean(html_str,
+                        tags=tags,
+                        attributes=attr,
+                        styles=styles,
+                        strip=strip)
