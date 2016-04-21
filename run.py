5ab9248ffd060f191cc9ea8034dfc8c600e648c1
Joel Castillo <jcastillo@records.nyc.gov>
2016-04-21 19:35:47 -0400

    Fixed issue with Run.py for local

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from os.path import isdir
 if isdir('.certs'):
     app.run(debug=True, port=8080, ssl_context=('.certs/openrecords.crt', '.certs/openrecords.key'))
 else:
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, port=8080, ssl_context='ad-hoc')
