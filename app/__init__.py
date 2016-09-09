523963391e8119d48b754361ca1c76c6b2ab8987
Joel Castillo <jcastillo@records.nyc.gov>
2016-09-09 18:53:04 -0400

    Removing DB from frontend branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,61 +1,29 @@
-import logging
-import os
-import time
-from datetime import timedelta
-from logging import Formatter
-from logging.handlers import RotatingFileHandler
-
-from flask import Flask, session
-from flask_kvsession import KVSessionExtension
-from flask_migrate import Migrate
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from simplekv.db.sql import SQLAlchemyStore
 
 from config import config
+from . import main
 
-migrate = Migrate()
 db = SQLAlchemy()
 
-
-def load_db(db):
-    db.create_all()
+app = Flask(__name__)
 
 
-def create_app(config_name):  # App Factory
-    app = Flask(__name__)
+def create_app(config_name):
+    """
+    Set up the Flask Application context.
+    :param config_name: Configuration for specific application context.
+    :return: Flask application
+    """
     app.config.from_object(config[config_name])
-
-    if os.environ.get('DATABASE_URL') is None:
-        app.config[
-            'SQLALCHEMY_DATABASE_URI'] = \
-            app.config.get('SQLALCHEMY_DATABASE_URI')
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
     config[config_name].init_app(app)
-    migrate.init_app(app, db)
+
     db.init_app(app)
-    with app.app_context():
-        load_db(db)
-        store = SQLAlchemyStore(db.engine, db.metadata, 'sessions')
-        kvsession = KVSessionExtension(store, app)
-        logfile_name = "{}/{}.log".format(app.config['LOGFILE_DIRECTORY'], time.strftime("%Y%m%d-%H%M%S"))
-        handler = RotatingFileHandler(logfile_name, maxBytes=10000, backupCount=1)
-        handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s '
-                                       '[in %(pathname)s:%(lineno)d]'))
-        handler.setLevel(logging.INFO)
-        app.logger.addHandler(handler)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
-
-    app.permanent_session_lifetime = timedelta(minutes=15)
-
-    @app.before_request
-    def func():
-        session.modified = True
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,24 +1,3 @@
-from sqlalchemy.dialects.postgresql import JSON
-
-from app import db
-
-
-class User(db.Model):
+class User():
     """User class which can be an agency user or a public user"""
-    __tablename__ = 'users'
-    guid = db.Column(db.String(1000), primary_key=True)
-    email = db.Column(db.String(254))
-    first_name = db.Column(db.String(32), nullable=False)
-    middle_initial = db.Column(db.String(1))
-    last_name = db.Column(db.String(64))
-    email_validated = db.Column(db.Boolean(), nullable=False)
-    terms_of_use_accepted = db.Column(db.Boolean())
-    user_type = db.Column(db.String(64))
-    title = db.Column(db.String(64))
-    company = db.Column(db.String(128))
-    phone_number = db.Column(db.String(15))
-    fax_number = db.Column(db.String(15))
-    mailing_address = db.Column(JSON)
-
-    def __repr__(self):
-        return '<Role {0!r}>'.format(self.guid + self.user_type)
+    pass
