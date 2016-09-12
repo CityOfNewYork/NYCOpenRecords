74ce7c9cbba65e32cbce3d2f7b6d9226b2e4a05d
Gary Zhou <gzhou@records.nyc.gov>
2016-09-12 16:11:52 -0400

    Uncommented main blueprint in init to run application

--- app/__init__.py
+++ app/__init__.py
@@ -23,11 +23,10 @@ def create_app(config_name):
 
     db.init_app(app)
 
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
-    #
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     return app
-
