035f18c9af2e3593996e96f071b176c4c6f9dc04
Joel Castillo <jcastillo@records.nyc.gov>
2016-09-17 16:22:39 -0400

    Merge OP-799 with OP-778.
    Combined frontend and backend of creating a request functionality.

--- app/request/views.py
+++ app/request/views.py
@@ -10,11 +10,10 @@ from flask import (
     redirect,
     url_for,
 )
-from app.request.forms import PublicUserRequestForm, AgencyUserRequestForm, AnonymousRequestForm
+
 from app.request import request_blueprint
+from app.request.forms import PublicUserRequestForm, AgencyUserRequestForm, AnonymousRequestForm
 from app.request.utils import process_request, process_anon_request, process_agency_request
-from werkzeug.utils import secure_filename
-import os
 
 
 @request_blueprint.route('/new/<string:user_type>', methods=['GET', 'POST'])
@@ -35,40 +34,30 @@ def create_request(user_type):
     if user_type == 'public':
         form = PublicUserRequestForm()
         if request.method == 'POST':
-            if form.validate_on_submit():
-                # Helper function to handle processing of data and secondary validation on the backend
-                process_request(title=form.request_title.data, description=form.request_description.data,
-                                submission=form.method_received.data)
-                return redirect(url_for('main.index'))
-            else:
-                print(form.errors)
-        return render_template('request/public_user_new_request.html', form=form)
+            # Helper function to handle processing of data and secondary validation on the backend
+            process_request(agency=form.request_agency.data, title=form.request_title.data, description=form.request_description.data)
+            return redirect(url_for('main.index'))
+        return render_template('request/new_request_user.html', form=form)
 
     # Anonymous user
     elif user_type == 'anon':
         form = AnonymousRequestForm()
         if request.method == 'POST':
-            if form.validate_on_submit():
-                process_anon_request(title=form.request_title.data, description=form.request_description.data,
-                                     submission=form.method_received.data, email=form.email.data,
-                                     first_name=form.first_name.data, last_name=form.last_name.data,
-                                     user_title=form.user_title.data, company=form.user_organization.data,
-                                     phone=form.phone.data, fax=form.fax.data, address=form.address.data)
-                return redirect(url_for('main.index'))
-            else:
-                print(form.errors)
-        return render_template('request/anon_user_new_request.html', form=form)
+            process_anon_request(agency=form.request_agency.data,title=form.request_title.data, description=form.request_description.data,
+                                 email=form.email.data,
+                                 first_name=form.first_name.data, last_name=form.last_name.data,
+                                 user_title=form.user_title.data, company=form.user_organization.data,
+                                 phone=form.phone.data, fax=form.fax.data, address=form.address.data)
+            return redirect(url_for('main.index'))
+        return render_template('request/new_request.html', form=form)
 
     elif user_type == 'agency':
         form = AgencyUserRequestForm()
         if request.method == 'POST':
-            if form.validate_on_submit():
-                process_agency_request(title=form.request_title.data, description=form.request_description.data,
-                                       submission=form.method_received.data, email=form.email.data,
-                                       first_name=form.first_name.data, last_name=form.last_name.data,
-                                       user_title=form.user_title.data, company=form.user_organization.data,
-                                       phone=form.phone.data, fax=form.fax.data, address=form.address.data)
-                return redirect(url_for('main.index'))
-            else:
-                print(form.errors)
-        return render_template('request/agency_user_new_request.html', form=form)
+            process_agency_request(agency=form.request_agency.data,title=form.request_title.data, description=form.request_description.data,
+                                   submission=form.method_received.data, email=form.email.data,
+                                   first_name=form.first_name.data, last_name=form.last_name.data,
+                                   user_title=form.user_title.data, company=form.user_organization.data,
+                                   phone=form.phone.data, fax=form.fax.data, address=form.address.data)
+            return redirect(url_for('main.index'))
+        return render_template('request/new_request_agency.html', form=form)
