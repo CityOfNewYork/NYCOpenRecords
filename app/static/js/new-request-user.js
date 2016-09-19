8187932df5e145bd5320e85f9f01549adb309b0d
August Tan <atan@records.nyc.gov>
2016-09-19 10:43:39 -0400

    fixed validators on front end

--- app/static/js/new-request-user.js
+++ app/static/js/new-request-user.js
@@ -2,13 +2,6 @@
  * Created by atan on 9/14/16.
  */
 $(document).ready(function () {
-    // Datepicker for date request was received when creating a new request
-    $(".dtpick").datepicker({
-        dateFormat: "yy-mm-dd"
-    });
-    // $('#request-form').parsley().subcribe('parsley:form:validate', function(formInstance){
-    // })
-
     // Apply parsley validation styles to the input forms for a new request.
     document.getElementById('request-title').setAttribute('data-parsley-required', '');
     document.getElementById('request-title').setAttribute('data-parsley-maxlength', 90);
