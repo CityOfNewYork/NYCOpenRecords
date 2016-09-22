42a901c955d13a93cc0e882e34aa2d4f2fd01fb2
Joel Castillo <jcastillo@records.nyc.gov>
2016-09-22 19:32:35 -0400

    Creating shell context

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 # manage.py
 import os
-from flask_script import Manager, Shell
+
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+
 from app import create_app, db
-from app.models import User, Agency
+from app.models import User, Agency, Request, Response, Event, Reason, Permission, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -11,7 +13,20 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Agency=Agency)
+    return dict(
+        app=app,
+        db=db,
+        User=User,
+        Agency=Agency,
+        Request=Request,
+        Response=Response,
+        Event=Event,
+        Reason=Reason,
+        Permission=Permission,
+        Role=Role
+    )
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
