6db149e500eacc33540b1264195aaedc3add1059
Gary Zhou <garyzhouny@gmail.com>
2016-09-26 17:56:43 -0400

    Added frontend files and email endpoint for test
    Added simple html page and email_template to test email functionality.
    Added email endpoint to call send_email function.
    Updated and cleaned requirements file.

--- app/main/views.py
+++ app/main/views.py
@@ -15,6 +15,10 @@ from flask import (
     redirect,
     session
 )
+from flask_mail import Message
+from app.email_utils import send_email
+from flask_wtf import Form
+from wtforms import SubmitField
 
 from app.models import Users
 from . import main
@@ -23,3 +27,18 @@ from . import main
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('base.html')
+
+
+class EmailForm(Form):
+    submit = SubmitField('Send Email')
+
+
+# Used for testing email functionality
+@main.route('/email', methods=['GET', 'POST'])
+def test_email():
+    form = EmailForm()
+    if request.method == 'POST':
+        # send email to [to, cc, bcc] with subject 'Test' and content from email_confirmation template
+        send_email('test@email.com', 'test2@email.com', 'test3@email.com', 'Test', 'email_templates/email_confirmation')
+        flash('Email sent')
+    return render_template('email.html', form=form)
