056ffe534e6dfe233afd2358458e4b9639f7e086
Joel Castillo <jcastillo@records.nyc.gov>
2016-09-27 13:33:03 -0400

    Add a celery command to the manage.py CLI tool Allows us to standardize setting up the celery worker for testing functionality. (feature/OP-824)[https://nycrecords.atlassian.net/browse/OP-824]

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # manage.py
 import os
+import subprocess
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
@@ -28,5 +29,11 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+@manager
+def celery():
+    subprocess.call(['celery', 'worker',  '-A', 'celery_worker.celery', '--loglevel=info'])
+
+
 if __name__ == "__main__":
     manager.run()
