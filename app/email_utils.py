440c4ea781df2d70c19e42aa822df4758a4afa53
Gary Zhou <garyzhouny@gmail.com>
2016-09-27 14:21:48 -0400

    Updated function for proper cc,bcc functionality

--- app/email_utils.py
+++ app/email_utils.py
@@ -40,7 +40,7 @@ def send_email(to, cc, bcc, subject, template, **kwargs):
     """
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to, cc, bcc])
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to], cc=[cc], bcc=[bcc])
     # msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     send_async_email.delay(msg)
