1e329e8e2b3f7f9ac3ca5a23e875e05f6136bc41
Brandon Tang <btang@Administrators-MacBook-Pro-3.local>
2016-09-30 17:01:12 -0400

    Fixed up formatting in action-buttons-mobile-toggle.js

--- app/static/js/request/action-buttons-mobile-toggle.js
+++ app/static/js/request/action-buttons-mobile-toggle.js
@@ -1,10 +1,9 @@
 $(window).resize(function () {
-       var width = $(window).width();
-       if (width >= 768) {
-           $('#mobile-toggle').addClass('tabs-left');
-       }
-       else {
-           $('#mobile-toggle').removeClass('tabs-left');
-       }
-   })
-   .resize();
+    var width = $(window).width();
+    if (width >= 768) {
+        $('#mobile-toggle').addClass('tabs-left');
+    }
+    else {
+        $('#mobile-toggle').removeClass('tabs-left');
+    }
+}).resize();
