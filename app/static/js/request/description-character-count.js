fbb5ea96fb939bf784ce1e1e73e188b5276c38d2
Jonathan Yu <jyu@records.nyc.gov>
2016-10-04 16:07:08 -0400

    changed character counter to count starting from max length, took out lib in git ignore

--- app/static/js/request/description-character-count.js
+++ app/static/js/request/description-character-count.js
@@ -1,3 +1,10 @@
-$('#request-description').keyup(function() { 
-        var length = $(this).val().length; 
-        $('#description-character-count').text(length+" / characters 5000 used"); });
+$('#request-description').keyup(function () {
+    var length = $(this).val().length;
+    var length = 5000 - length;
+    $('#description-character-count').text(length + " characters remaining");
+    if (length == 0) {
+        document.getElementById("description-character-count").style.color = "red";
+    } else {
+        document.getElementById("description-character-count").style.color = "black";
+    }
+});
--- app/static/js/request/title-character-count.js
+++ app/static/js/request/title-character-count.js
@@ -1,3 +1,10 @@
-$('#request-title').keyup(function() { 
-        var length = $(this).val().length; 
-        $('#title-character-count').text(length+" / 90 characters used"); });
+$('#request-title').keyup(function () {
+    var length = $(this).val().length;
+    var length = 90 - length;
+    $('#title-character-count').text(length + " characters remaining");
+    if (length == 0) {
+        document.getElementById("title-character-count").style.color = "red";
+    } else {
+        document.getElementById("title-character-count").style.color = "black";
+    }
+});
