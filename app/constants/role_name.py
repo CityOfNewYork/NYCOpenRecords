d4077983f2c7ee77939b3a016c440c06e282b0d6
Palisand <p9alisand@gmail.com>
2016-10-08 20:04:14 -0400

    remove unused set

--- app/constants/role_name.py
+++ app/constants/role_name.py
@@ -4,12 +4,3 @@ PUBLIC_REQUESTER = 'Public User - Requester'
 AGENCY_HELPER = 'Agency Helper'
 AGENCY_OFFICER = 'Agency FOIL Officer'
 AGENCY_ADMIN = 'Agency Administrator'
-
-ALL = frozenset((
-    ANONYMOUS,
-    PUBLIC_NON_REQUESTER,
-    PUBLIC_REQUESTER,
-    AGENCY_HELPER,
-    AGENCY_OFFICER,
-    AGENCY_ADMIN
-))
