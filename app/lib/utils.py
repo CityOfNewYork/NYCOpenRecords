9d335f9354a81c59542470cb125c7b0892667d73
Palisand <p9alisand@gmail.com>
2016-10-12 10:43:28 -0400

    encode request filename

--- app/lib/utils.py
+++ app/lib/utils.py
@@ -3,6 +3,8 @@
 
 """
 
+from base64 import b64decode
+
 class InvalidUserException(Exception):
 
     def __init__(self, user):
@@ -11,3 +13,16 @@ class InvalidUserException(Exception):
         """
         super(InvalidUserException, self).__init__(
             "Invalid user: {}".format(user))
+
+
+def b64decode_lenient(data):
+    """
+    Decodes base64 (bytes or str), padding being optional
+
+    :param data: a string or bytes-like object of base64 data
+    :return: a decoded string
+    """
+    if type(data) is str:
+        data = data.encode()
+    data += b'=' * (4 - (len(data) % 4))
+    return b64decode(data).decode()
