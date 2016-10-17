5beb3f8fe833ff7e6c6e94f60323a99d03cb591f
Gary Zhou <garyzhouny@gmail.com>
2016-10-17 14:01:10 -0400

    Implemented extension select field and datepicker
    Added data required parsley validation for select field.
    Working on frontend div and buttons implementation.

--- app/static/js/request/view_extension.js
+++ app/static/js/request/view_extension.js
@@ -2,16 +2,25 @@ $(document).ready(function () {
     $(".add-extension .Div1").each(function (e) {
         if (e != 0)
             $(this).hide();
+        else
+            $("#previous").hide();
     });
 
     $("#next").click(function () {
-        if ($(".add-extension .Div1:visible").next().length != 0)
-            $(".add-extension .Div1:visible").next().show().prev().hide();
-        else {
-            $(".add-extension .Div1:visible").hide();
-            $(".add-extension .Div1:first").show();
+        if ($('#extension-select').parsley().isValid() == false) {
+            $('#extension-select').parsley().validate();
+            preventDefault();
+            // e.stopPropagation();
         }
-        return false;
+            if ($(".add-extension .Div1:visible").next().length != 0) {
+                $(".add-extension .Div1:visible").next().show().prev().hide();
+                $("#previous").show();
+            }
+            else {
+                $(".add-extension .Div1:visible").hide();
+                $(".add-extension .Div1:first").show();
+            }
+            return false;
     });
 
     $("#previous").click(function () {
@@ -24,7 +33,21 @@ $(document).ready(function () {
         return false;
     });
 
+    $("#extension-select").change(function () {
+        selected = $(this).val();
+        if (selected === "-1") {
+            $("#custom_due_date").show();
+        }
+        else {
+            $("#custom_due_date").hide();
+        }
+    });
+
     $("#dtpick").datepicker({
         dateFormat: "yy-mm-dd"
     });
+
+    $('#extension-select').attr('data-parsley-required', '');
+    $('#extension-select').attr('data-parsley-required-message', 'Extension length must be selected');
+
 });
