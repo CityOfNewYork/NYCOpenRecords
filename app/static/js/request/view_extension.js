7de7243b34c01755b98d41aed6c3d43a9f8984b8
Gary Zhou <garyzhouny@gmail.com>
2016-10-17 16:04:21 -0400

    Fixed documentation and changed if to else if

--- app/static/js/request/view_extension.js
+++ app/static/js/request/view_extension.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // Hides all other dives except for the first. Also hides previous button on the first div.
+    // Hides all other divs except for the first. Also hides previous button on the first div.
     $(".add-extension .Div1").each(function (e) {
         if (e != 0)
             $(this).hide();
@@ -9,32 +9,34 @@ $(document).ready(function () {
 
     // Handles click events on the next button
     $("#next").click(function (e) {
-        if ($('#extension-select').parsley().isValid() == false){
+        // Validates extension select field on next button click
+        if ($('#extension-select').parsley().isValid() == false) {
             $('#extension-select').parsley().validate();
             e.preventDefault();
-            e.stopImmediatePropagation();
             return false
         }
-        if ($('#dtpick').parsley().isValid() == false) {
+        // Validates custom datepicker on next button click
+        else if ($('#dtpick').parsley().isValid() == false) {
             $('#dtpick').parsley().validate();
             e.preventDefault();
-            e.stopImmediatePropagation();
             return false
         }
-            if ($(".add-extension .Div1:visible").next().length != 0) {
-                $('#dtpick').parsley().reset();
-                $(".add-extension .Div1:visible").next().show().prev().hide();
-                $("#previous").show();
-            }
-            else {
-                $(".add-extension .Div1:visible").hide();
-                $(".add-extension .Div1:first").show();
-            }
-            return false;
+        // Proceeds to next div on next button click
+        else if ($(".add-extension .Div1:visible").next().length != 0) {
+            $('#dtpick').parsley().reset();
+            $(".add-extension .Div1:visible").next().show().prev().hide();
+            $("#previous").show();
+        }
+        else {
+            $(".add-extension .Div1:visible").hide();
+            $(".add-extension .Div1:first").show();
+        }
+        return false;
     });
 
     // Handles click events on the previous button
     $("#previous").click(function () {
+        // Proceeds to previous div on previous button click
         if ($(".add-extension .Div1:visible").prev().length != 0)
             $(".add-extension .Div1:visible").prev().show().next().hide();
         else {
@@ -61,7 +63,7 @@ $(document).ready(function () {
     });
 
     // Apply data-parsley-required attribute to specified fields
-    $('#dtpick').attr('data-parsley-required','');
+    $('#dtpick').attr('data-parsley-required', '');
     $('#extension-select').attr('data-parsley-required', '');
 
     // Custom validation messages
