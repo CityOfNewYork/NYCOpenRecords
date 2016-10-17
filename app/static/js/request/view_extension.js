6cc4ac307f804181aed39eb54bfa0befb4ae4132
Gary Zhou <garyzhouny@gmail.com>
2016-10-17 19:47:40 -0400

    Updated buttons to bootstrap buttons and aligned
    Next buttons are now aligned to the right and previous to the left.
    Renamed class and id names of elements for the frontend.

--- app/static/js/request/view_extension.js
+++ app/static/js/request/view_extension.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
             $("#previous").hide();
     });
 
-    // Handles click events on the next button
-    $("#next-btn1").click(function (e) {
+    // Handles click events on the first next button
+    $("#next-1").click(function (e) {
         if ($("#custom-extension").is(':visible')) {
             $("#custom-extension").parsley().validate();
             if (!$('#custom-extension').parsley().isValid()) {
@@ -23,41 +23,45 @@ $(document).ready(function () {
         }
     });
 
-    $("#next-btn2").click(function () {
+    // Handles click events on the second next button
+    $("#next-2").click(function () {
         document.getElementById("second").style.display = "none";
         document.getElementById("third").style.display = "block";
     });
 
-    $("#prev-btn1").click(function() {
+    // Handles click events on the first previous button
+    $("#prev-1").click(function () {
         document.getElementById("first").style.display = "block";
         document.getElementById("second").style.display = "none";
     });
 
-    $("#prev-btn2").click(function() {
+    // Handles click events on the second previous button
+    $("#prev-2").click(function () {
         document.getElementById("third").style.display = "none";
         document.getElementById("second").style.display = "block";
     });
-// Shows custom due date datepicker when Custom Due Date is selected
-$("#extension-select").change(function () {
-    selected = $(this).val();
-    if (selected === "-1") {
-        $("#custom_due_date").show();
-    }
-    else {
-        $("#custom_due_date").hide();
-    }
-});
 
-// Datepicker for extension date of a request
-$("#custom-extension").datepicker({
-    dateFormat: "yy-mm-dd"
-});
+    // Shows custom due date datepicker when Custom Due Date is selected
+    $("#extension-select").change(function () {
+        selected = $(this).val();
+        if (selected === "-1") {
+            $("#custom_due_date").show();
+        }
+        else {
+            $("#custom_due_date").hide();
+        }
+    });
 
-// Apply parsley validation styles to input fields of adding an extension
-$('#custom-extension').attr('data-parsley-required', '');
-$('#extension-select').attr('data-parsley-required', '');
+    // Datepicker for extension date of a request
+    $("#custom-extension").datepicker({
+        dateFormat: "yy-mm-dd"
+    });
 
-// Apply custom validation messages
-$('#custom-extension').attr('data-parsley-required-message', 'Extension date must be chosen');
-$('#extension-select').attr('data-parsley-required-message', 'Extension length must be selected');
+    // Apply parsley validation styles to input fields of adding an extension
+    $('#custom-extension').attr('data-parsley-required', '');
+    $('#extension-select').attr('data-parsley-required', '');
+
+    // Apply custom validation messages
+    $('#custom-extension').attr('data-parsley-required-message', 'Extension date must be chosen');
+    $('#extension-select').attr('data-parsley-required-message', 'Extension length must be selected');
 });
