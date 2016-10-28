1ca1e4d5571002d856065c5a2e90186df2aea980
Jonathan Yu <jyu@records.nyc.gov>
2016-10-28 12:16:07 -0400

    initial commit, added if fix for elastic search error in develop

--- config.py
+++ config.py
@@ -71,6 +71,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
                                'postgresql://localhost:5432/openrecords_v2_0_dev')
     # Using Vagrant? Try: 'postgresql://vagrant@/openrecords_v2_0_dev'
+    ELASTICSEARCH_ENABLED = eval(str(os.environ.get('ELASTICSEARCH_ENABLED'))) or False
 
 
 class TestingConfig(Config):
