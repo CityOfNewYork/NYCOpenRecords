401249c1b94bc847304f9dbde824e7415b415862
Palisand <p9alisand@gmail.com>
2016-11-08 17:01:22 -0500

    add tests

--- app/response/views.py
+++ app/response/views.py
@@ -382,15 +382,16 @@ def get_response_content(response_id):
             upload_path,
             response.metadatas.name
         )
+        filepath = os.path.join(*filepath_parts)
         token = flask_request.args.get('token')
         if token is not None:
             resptok = ResponseTokens.query.filter_by(
                 token=token, response_id=response_id).first()
             if resptok is not None:
                 if datetime.utcnow() < resptok.expiration_date:
-                    if os.path.exists(os.path.join(*filepath_parts)):
-                        return send_from_directory(*filepath_parts,
-                                                   as_attachment=True)
+                    if os.path.exists(filepath):
+                        return send_from_directory(
+                            *filepath_parts, as_attachment=True)
                 else:
                     delete_object(resptok)
         else:
@@ -399,10 +400,10 @@ def get_response_content(response_id):
                    and UserRequests.query.filter_by(
                         request_id=response.request_id,
                         user_guid=current_user.guid,
-                        auth_user_type=current_user.auth_user_type
-                   ) is not None):
-                        return send_from_directory(*filepath_parts,
-                                                   as_attachment=True)
+                        auth_user_type=current_user.auth_user_type).first() is not None
+                   and os.path.exists(filepath)):
+                        return send_from_directory(
+                            *filepath_parts, as_attachment=True)
             else:
                 return redirect(url_for(
                     'auth.index',
