9e311ee354fd9d71293ecc977dd28ee456ffea0e
Gary Zhou <garyzhouny@gmail.com>
2016-11-18 11:41:30 -0500

    Updated input file change event
    On file upload change, display filename.
    On cancel event in upload window, do nothing unless browser is chrome in which case filename would be set to empty due to chrome overwriting the file input.

--- app/static/js/request/new-request-user.js
+++ app/static/js/request/new-request-user.js
@@ -43,9 +43,21 @@ $(document).ready(function () {
     // Limit the size of the file upload to 20 Mb. Second parameter is number of Mb's.
     $('#request-file').attr('data-parsley-max-file-size',"20");
 
-    // Set name of the file to filename div
-    $("#request-file").on('change', function () {
-        $("#filename").text((this.files[0].name));
+    // Set name of the file to the text of filename div if file exists
+    $("#request-file").change(function () {
+        var file = this.files[0];
+        var isChrome = window.chrome;
+
+        if(file) {
+            $("#filename").text((this.files[0].name));
+        }
+        // Cancel is clicked on upload window
+        else {
+            // If browser is chrome, reset filename text
+            if(isChrome) {
+                $("#filename").text("");
+            }
+        }
     });
 
     // Clear the file from input and the name from filename div
