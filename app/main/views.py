990ee296eb017ee028ebf0af2fe5d435d092068c
sinsang <stefanick96@gmail.com>
2016-12-14 11:55:58 -0500

    Created FAQ page

--- app/main/views.py
+++ app/main/views.py
@@ -58,9 +58,15 @@ def status():
     return 200
 
 
+@main.route('/faq.html', methods=['GET'])
+def faq():
+    return render_template('main/faq.html')
+
+
 @main.route('/about', methods=['GET'])
 def about():
     return render_template('main/about.html')
+
 
 @main.route('/login')
 @main.route('/login/<guid>', methods=['GET'])
@@ -79,6 +85,7 @@ def login(guid=None):
 
     return render_template('main/test/user_list.html', users=users, current_user=current_user)
 
+
 #
 # @main.route('/login-user/<guid>', methods=['GET'])
 # def test_specific_user(guid=None):
@@ -89,7 +96,7 @@ def login(guid=None):
 @main.route('/logout-user/<guid>', methods=['GET'])
 def logout(guid):
     if not guid:
-        return(redirect(url_for('main.login')))
+        return (redirect(url_for('main.login')))
     user = Users.query.filter_by(guid=guid).one()
     logout_user()
     flash('Logged out user: {}'.format(user.auth_user_type))
