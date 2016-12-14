d5d219f335c35c07aae5f3f0c215b02b8a6330d7
sinsang <stefanick96@gmail.com>
2016-12-14 17:38:21 -0500

    Added parsley validation

---
+++ app/static/js/request/contact.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sinsang on 12/14/16.
+//  */
+// $(document).ready(function () {
+//
+//
+//     $('#name').attr('data-parsley-required-message','');
+//     $('#email').attr('data-parsley-required-message', '');
+//     $('#subject').attr('data-parsley-required-message', '');
+//     $('#message').attr('data-parsley-required-message', '');
+//
+//     //Custom Validation Messages
+//     $('#name').attr('data-parsley-required-message', 'This field cannot be left blank')
+//
+// })
+// //Activate on
+// $('#contact-info').parsley().validate();
