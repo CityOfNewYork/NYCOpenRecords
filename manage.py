758f61955ae28f5935e274e3b2558073642a2e8d
Joel Castillo <jcastillo@records.nyc.gov>
2016-12-16 07:40:37 -0500

    Added create_user to create user of specified type in database from cli

--- manage.py
+++ manage.py
@@ -140,5 +140,50 @@ def create_users():
         print("Created User: {guid} - {name} ({email})".format(guid=user.guid, name=user.name, email=user.email))
 
 
+@manager.option("-a", "--agency", help="Create agency user.", action="store_true", dest='agency')
+@manager.option("-g", "--google", help="Create google user.", action="store_true", dest='google')
+@manager.option("-y", "--yahoo", help="Create yahoo user.", action="store_true", dest='yahoo')
+@manager.option("-f", "--facebook", help="Create facebook user.", action="store_true", dest='facebook')
+@manager.option("-l", "--linkedin", help="Create linkedin user.", action="store_true", dest='linkedin')
+@manager.option("-m", "--microsoft", help="Create microsoft user.", action="store_true", dest='microsoft')
+@manager.option("-e", "--edirsso", help="Create edirssoo user.", action="store_true", dest='edirsso')
+def create_user(agency=False,
+                google=False,
+                yahoo=False,
+                facebook=False,
+                linkedin=False,
+                microsoft=False,
+                edirsso=False):
+    """Create a user of the specified type. Defaults to an anonymous user."""
+    from tests.lib.tools import create_user
+    from app.constants.user_type_auth import (
+        AGENCY_USER,
+        PUBLIC_USER_NYC_ID,
+        PUBLIC_USER_FACEBOOK,
+        PUBLIC_USER_LINKEDIN,
+        PUBLIC_USER_GOOGLE,
+        PUBLIC_USER_YAHOO,
+        PUBLIC_USER_MICROSOFT
+    )
+    if agency:
+        user = create_user(AGENCY_USER)
+    if edirsso:
+        user = create_user(PUBLIC_USER_NYC_ID)
+    if facebook:
+        user = create_user(PUBLIC_USER_FACEBOOK)
+    if linkedin:
+        user = create_user(PUBLIC_USER_LINKEDIN)
+    if google:
+        user = create_user(PUBLIC_USER_GOOGLE)
+    if yahoo:
+        user = create_user(PUBLIC_USER_YAHOO)
+    if microsoft:
+        user = create_user(PUBLIC_USER_MICROSOFT)
+    if not (facebook or google or linkedin or microsoft or yahoo or agency or edirsso):
+        user = create_user()
+
+    print(user, "created.")
+
+
 if __name__ == "__main__":
     manager.run()
