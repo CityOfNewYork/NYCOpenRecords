a80f9b9ef134cbe1dffc4aeb1e06d8049a43a545
Gary Zhou <gzhou@records.nyc.gov>
2017-03-21 15:53:33 -0400

    ResponseUtilsTests WIP, generic add_closing works

--- tests/responses/views.py
+++ tests/responses/views.py
@@ -1,4 +1,5 @@
 from flask import url_for
+from flask_login import login_user, logout_user
 from unittest.mock import patch
 from urllib.parse import urlparse
 
@@ -11,6 +12,7 @@ from tests.lib.tools import (
 )
 
 from app.constants import request_status
+from app.response.utils import add_closing
 
 
 class ResponseViewsTests(BaseTestCase, TestHelpers):
@@ -22,27 +24,26 @@ class ResponseViewsTests(BaseTestCase, TestHelpers):
         self.admin_860 = uf.create_agency_admin(agency_ein=self.agency_ein_860)
         self.rf = RequestFactory
         self.rf_agency_860 = RequestFactory(agency_ein=self.agency_ein_860)
+        self.request = self.rf_agency_860.create_request_as_public_user()
 
     def test_response_denial(self):
-        request = self.rf_agency_860.create_request_as_public_user()
         with self.client as client:
             login_user_with_client(client, self.admin_860.get_id())
             response = self.client.post(
-                '/response/denial/' + request.id,
+                '/response/denial/' + self.request.id,
                 data={
                     "reasons": ['1', '2', '3'],
                     "email-summary": 'This is a email summary'
                 }
             )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=request.id))
+        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=self.request.id))
 
     def test_response_denial_missing_reasons(self):
-        request = self.rf_agency_860.create_request_as_public_user()
         with self.client as client:
             login_user_with_client(client, self.admin_860.get_id())
             response = self.client.post(
-                '/response/denial/' + request.id,
+                '/response/denial/' + self.request.id,
                 data={
                     "email-summary": 'This is a email summary'
                 }
@@ -50,32 +51,30 @@ class ResponseViewsTests(BaseTestCase, TestHelpers):
         self.assert_flashes(expected_message='Uh Oh, it looks like the denial reasons is missing! '
                                              'This is probably NOT your fault.', expected_category='danger')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=request.id))
+        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=self.request.id))
 
     def test_response_closing(self):
-        request = self.rf_agency_860.create_request_as_public_user()
-        request.acknowledge(days=30)
-        request.set_agency_description(agency_description='blah')
-        request.set_agency_description_privacy(privacy=False)
+        self.request.acknowledge(days=30)
+        self.request.set_agency_description(agency_description='blah')
+        self.request.set_agency_description_privacy(privacy=False)
         with self.client as client:
             login_user_with_client(client, self.admin_860.get_id())
             response = self.client.post(
-                '/response/closing/' + request.id,
+                '/response/closing/' + self.request.id,
                 data={
                     "reasons": ['1', '2', '3'],
                     "email-summary": 'This is a email summary'
                 }
             )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=request.id))
+        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=self.request.id))
 
     def test_response_closing_missing_reasons(self):
-        request = self.rf_agency_860.create_request_as_public_user()
-        request.acknowledge(days=30)
+        self.request.acknowledge(days=30)
         with self.client as client:
             login_user_with_client(client, self.admin_860.get_id())
             response = self.client.post(
-                '/response/closing/' + request.id,
+                '/response/closing/' + self.request.id,
                 data={
                     "email-summary": 'This is a email summary'
                 }
@@ -83,15 +82,14 @@ class ResponseViewsTests(BaseTestCase, TestHelpers):
         self.assert_flashes(expected_message='Uh Oh, it looks like the closing reasons is missing! '
                                              'This is probably NOT your fault.', expected_category='danger')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=request.id))
+        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=self.request.id))
 
     def test_response_closing_no_agency_description(self):
-        request = self.rf_agency_860.create_request_as_public_user()
-        request.acknowledge(days=30)
+        self.request.acknowledge(days=30)
         with self.client as client:
             login_user_with_client(client, self.admin_860.get_id())
             response = self.client.post(
-                '/response/closing/' + request.id,
+                '/response/closing/' + self.request.id,
                 data={
                     "reasons": ['1', '2', '3'],
                     "email-summary": 'This is a email summary'
@@ -99,5 +97,25 @@ class ResponseViewsTests(BaseTestCase, TestHelpers):
             )
         self.assert_flashes(expected_message='Unable to close request:', expected_category='danger')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=request.id))
+        self.assertEqual(urlparse(response.location).path, url_for('request.view', request_id=self.request.id))
 
+
+class ResponseUtilsTests(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.agency_ein_860 = "0860"
+        uf = UserFactory()
+        self.admin_860 = uf.create_agency_admin(agency_ein=self.agency_ein_860)
+        self.rf = RequestFactory()
+        self.rf_agency_860 = RequestFactory(agency_ein=self.agency_ein_860)
+        self.request = self.rf_agency_860.create_request_as_public_user()
+
+    def test_add_closing(self):
+        login_user(self.admin_860)
+        self.request.acknowledge(days=30)
+        self.request.set_agency_description(agency_description='blah')
+        self.request.set_agency_description_privacy(privacy=False)
+        reasons = ['1', '2', '3']
+        add_closing(self.request.id, reasons, 'email body')
+        self.assertEqual(self.request.status, request_status.CLOSED)
+        logout_user()
