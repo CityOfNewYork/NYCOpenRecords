fa3793ed881f8e50191354b6c7298507920728e2
Gary Zhou <gzhou@records.nyc.gov>
2017-10-13 11:09:23 -0400

    Add start and test commands for logstash

--- build_scripts/elk_setup/logstash/setup.sh
+++ build_scripts/elk_setup/logstash/setup.sh
@@ -3,3 +3,9 @@
 # Download and install Logstash
 wget https://artifacts.elastic.co/downloads/logstash/logstash-5.6.2.rpm -P /tmp
 rpm -ivh /tmp/logstash-5.6.2.rpm
+
+# Start Logstash
+sudo initctl start logstash
+
+# Command to test Logstash
+# sudo /usr/share/logstash/bin/logstash --path.settings=/etc/logstash -e 'input { stdin { } } output { stdout {} }'
