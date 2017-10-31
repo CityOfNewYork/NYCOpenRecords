a72fec8b565430d1404ddffa397004bc421e3920
Gary Zhou <gzhou@records.nyc.gov>
2017-10-31 17:11:23 -0400

    Complete configuration for Filebeat/Logstash/ElasticSearch

--- build_scripts/elk_setup/logstash/setup.sh
+++ build_scripts/elk_setup/logstash/setup.sh
@@ -4,6 +4,11 @@
 wget https://artifacts.elastic.co/downloads/logstash/logstash-5.6.2.rpm -P /tmp
 rpm -ivh /tmp/logstash-5.6.2.rpm
 
+ln -s /vagrant/build_scripts/elk_setup/logstash/logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
+
+# logstash-filter-geoip offline plugin pack to be provided
+# /usr/share/logstash/bin/logstash-plugin install file:///vagrant/logstash-filter-geoip.zip
+
 # Start Logstash
 sudo initctl start logstash
 
