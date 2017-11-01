0612cceda0363b2414e4e6f0ded3177dee1bd9b3
Jonathan Yu <jyu@records.nyc.gov>
2017-11-01 15:47:07 -0400

    Fixed path for sites in nginx.conf

--- build_scripts/consul_setup/consul_setup.sh
+++ build_scripts/consul_setup/consul_setup.sh
@@ -6,5 +6,5 @@ wget https://releases.hashicorp.com/consul/1.0.0/consul_1.0.0_linux_amd64.zip
 unzip consul_1.0.0_linux_amd64.zip
 sudo mv consul /usr/bin/
 
-# Start Consul server
+# Start Consul server with this command after build scripts are finished running
 # sudo consul agent -server -bootstrap -data-dir /tmp/consul -bind 10.0.0.5 -ui
