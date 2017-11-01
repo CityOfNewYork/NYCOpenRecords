4e296d1650641c1a48fb005327ddbad8c369cdaa
Jonathan Yu <jyu@records.nyc.gov>
2017-11-01 14:59:40 -0400

    updated paths for nginx setup

--- build_scripts/consul_setup/consul_setup.sh
+++ build_scripts/consul_setup/consul_setup.sh
@@ -2,9 +2,9 @@
 
 # Install Consul binaries
 cd /tmp/
-wget https://releases.hashicorp.com/consul/1.0.0/consul_1.0.0_linux_amd64.zip?_ga=2.225939090.948864614.1509545805-1112607485.1509545805
+wget https://releases.hashicorp.com/consul/1.0.0/consul_1.0.0_linux_amd64.zip
 unzip consul_1.0.0_linux_amd64.zip
-sudo mv consul /bin/
+sudo mv consul /usr/bin/
 
 # Start Consul server
-sudo consul agent -server -bootstrap -data-dir /tmp/consul -bind 10.0.0.5 -ui
+# sudo consul agent -server -bootstrap -data-dir /tmp/consul -bind 10.0.0.5 -ui
--- build_scripts/consul_setup/nginx/nginx_setup.sh
+++ build_scripts/consul_setup/nginx/nginx_setup.sh
@@ -13,12 +13,12 @@ bash -c "printf '#\!/bin/bash\nsource /opt/rh/rh-nginx18/enable\n' > /etc/profil
 mv /etc/opt/rh/rh-nginx18/nginx/nginx.conf /etc/opt/rh/rh-nginx18/nginx/nginx.conf.orig
 
 # 5. SymLink nginx.conf
-ln -s /vagrant/build_scripts/consul_setup/nginx_conf/nginx.conf /etc/opt/rh/rh-nginx18/nginx/nginx.conf
+ln -s /vagrant/build_scripts/consul_setup/nginx/nginx_conf/nginx.conf /etc/opt/rh/rh-nginx18/nginx/nginx.conf
 
 # 6. Create ssl Certs
 openssl req \
        -newkey rsa:4096 -nodes -keyout /vagrant/build_scripts/consul_setup/consul_dev.key \
-       -x509 -days 365 -out /vagrant/build_scripts/consul_setup/consul_dev.crt -subj "/C=US/ST=New York/L=New York/O=NYC Department of Records and Information Services/OU=IT/CN=openrecords_v2_0.nyc"
+       -x509 -days 365 -out /vagrant/build_scripts/consul_setup/consul_dev.crt -subj "/C=US/ST=New York/L=New York/O=NYC Department of Records and Information Services/OU=IT/CN=consul"
 openssl x509 -in /vagrant/build_scripts/consul_setup/consul_dev.crt -out /vagrant/build_scripts/consul_setup/consul_dev.pem -outform PEM
 
 # 7. Restart Nginx
