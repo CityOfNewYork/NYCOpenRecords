fa8aa32c986c3738ccb116d7f9426f642f46e6aa
Gary Zhou <gzhou@records.nyc.gov>
2017-11-06 12:43:40 -0500

    Comments and minor changes

--- build_scripts/filebeat_setup/app_nginx/setup.sh
+++ build_scripts/filebeat_setup/app_nginx/setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# 1. Download and install Filebeat
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.6.2-x86_64.rpm -P /tmp
 rpm -ivh /tmp/filebeat-5.6.2-x86_64.rpm
 
 cp /vagrant/build_scripts/filebeat_setup/app_nginx/filebeat.yml /etc/filebeat/
 
+# 2. Get Logstash certificate for SSL communication and place in /data/ssl/
+# For Dev, run logstash/setup.sh first to get certificate filebeat_setup directory
+cp /vagrant/build_scripts/filebeat_setup/logstash_dev.crt /data/ssl/
+
+# 3. Start Filebeat
 /etc/init.d/filebeat start
 
-# Allow vagrant user to edit filebeat.yml
+# Add line to sudoers to allow vagrant user to edit filebeat.yml
 # vagrant ALL = (root) NOPASSWD: sudoedit /etc/filebeat/filebeat.yml
