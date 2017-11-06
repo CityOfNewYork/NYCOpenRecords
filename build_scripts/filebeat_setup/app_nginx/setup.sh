86a3724a9450131f30cf233a9006946e7cf21780
Gary Zhou <gzhou@records.nyc.gov>
2017-11-06 11:26:25 -0500

    Complete Logstash and Nginx integration

--- build_scripts/filebeat_setup/app_nginx/setup.sh
+++ build_scripts/filebeat_setup/app_nginx/setup.sh
@@ -3,9 +3,9 @@
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.6.2-x86_64.rpm -P /tmp
 rpm -ivh /tmp/filebeat-5.6.2-x86_64.rpm
 
-cp /vagrant/build_scripts/filebeat_setup/nginx/filebeat.yml /etc/filebeat/
+cp /vagrant/build_scripts/filebeat_setup/app_nginx/filebeat.yml /etc/filebeat/
 
 /etc/init.d/filebeat start
 
-# Allow vagrant user to edit the file
+# Allow vagrant user to edit filebeat.yml
 # vagrant ALL = (root) NOPASSWD: sudoedit /etc/filebeat/filebeat.yml
