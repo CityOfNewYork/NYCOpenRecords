c36c2b7fcc520da4cd654b430b2b5001d8b8250e
Joel Castillo <jocastillo@records.nyc.gov>
2017-11-08 18:39:49 -0500

    Changes to Filebeat and ELK Setups
    - Using mkdir -p instead of mkdir to prevent failing if a directory already exists
    - Using flags to handle passing in packages that need to be installed
    - Moved logstash persistent data to /data/logstash
    - Created missing /data/nginx_logs on ELK server
    - Added filebeat sudoers file in /etc/sudoers.d
    - Added env field to filebeat for output.
    - Retaining original filebeat.yml on App Nginx
    - Split install and setup scripts for filebeat to allow setup to be completed without root access.
    - Added trigger to remove subscriptions on vagrant destroy to Vagrantfile.example
    Outstanding issues:
    - Filebeat is throwing the following error on startup for the App Nginx server:
    (openrecords)[vagrant@localhost vagrant]$ /etc/init.d/filebeat start
    Starting filebeat: filebeat2017/11/08 23:36:32.901502 beat.go:346: CRIT Exiting: error loading config file: yaml: line 26: found unknown escape character
    Exiting: error loading config file: yaml: line 26: found unknown escape character
    
    Signed-off-by: Joel Castillo <jocastillo@records.nyc.gov>

--- build_scripts/elk_setup/logstash/setup.sh
+++ build_scripts/elk_setup/logstash/setup.sh
@@ -16,14 +16,17 @@ sed -i '/^\[ v3_ca \]/a subjectAltName = IP: 10.0.0.4' /tmp/logstash_dev.cnf
 openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout /vagrant/build_scripts/elk_setup/logstash/logstash_dev.key -out /vagrant/build_scripts/elk_setup/logstash/logstash_dev.crt -config /tmp/logstash_dev.cnf -subj "/C=US/ST=New York/L=New York/O=NYC Department of Records and Information Services/OU=IT/CN=logstash.dev"
 
 # 4. Transfer certificate to server with Filebeat
-cp /tmp/logstash_dev.crt /vagrant/build_scripts/filebeat_setup/  # for Dev
+cp /vagrant/build_scripts/elk_setup/logstash/logstash_dev.crt /vagrant/build_scripts/filebeat_setup/  # for Dev
 
-mkdir /data/ssl
+mkdir -p /data/ssl
 mv /vagrant/build_scripts/elk_setup/logstash/logstash_dev.key /data/ssl
 chmod 400 /data/ssl/logstash_dev.key
 mv /vagrant/build_scripts/elk_setup/logstash/logstash_dev.crt /data/ssl
 chmod 400 /data/ssl/logstash_dev.crt
 chown -R logstash /data/ssl/logstash_dev.*
+
+mkdir -p /data/logstash
+chown -R logstash /data/logstash
 
 # 5. Install logstash-filter-geoip plugin, offline plugin pack to be provided
 # /usr/share/logstash/bin/logstash-plugin install file:///vagrant/logstash-filter-geoip.zip
--- build_scripts/filebeat_setup/app_nginx/setup.sh
+++ build_scripts/filebeat_setup/app_nginx/setup.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-# 1. Download and install Filebeat
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.6.2-x86_64.rpm -P /tmp
-rpm -ivh /tmp/filebeat-5.6.2-x86_64.rpm
+function usage {
+   cat << EOF
+Usage: setup.sh -u username -h host
 
-cp /vagrant/build_scripts/filebeat_setup/app_nginx/filebeat.yml /etc/filebeat/
+Set's up filebeat with SSL Server-to-Server authentication to the Logstash aggregator.
+-u Username: Username for the logstash server
+-h Hostname: Hostanme of the logstash server
+
+EOF
+   exit 1
+}
+
+if [[ $# -ne 4 ]]; then
+    usage;
+fi
+
+if [[ "$1" != "-u" ]]; then
+    usage;
+fi
+
+if [[ "$3" != "-h" ]]; then
+    usage;
+fi
+
+username=$2
+hostname=$4
 
 # 2. Get Logstash certificate for SSL communication and place in /data/ssl/
 # For Dev, run logstash/setup.sh first to get certificate filebeat_setup directory
-cp /vagrant/build_scripts/filebeat_setup/logstash_dev.crt /data/ssl/
+scp ${username}@${hostname}:/vagrant/build_scripts/filebeat_setup/logstash_dev.crt /data/ssl/
 
 # 3. Start Filebeat
 /etc/init.d/filebeat start
-
-# Add line to sudoers to allow vagrant user to edit filebeat.yml
-# vagrant ALL = (root) NOPASSWD: sudoedit /etc/filebeat/filebeat.yml
