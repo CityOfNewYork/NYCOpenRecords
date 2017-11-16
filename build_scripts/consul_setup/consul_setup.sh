93832fb8910f4e35c51d53df4488c9ce6411c594
Jonathan Yu <jyu@records.nyc.gov>
2017-11-16 11:01:33 -0500

    Removed comment

--- build_scripts/consul_setup/consul_setup.sh
+++ build_scripts/consul_setup/consul_setup.sh
@@ -33,7 +33,6 @@ openssl req -x509 -newkey rsa:2048 -days 365 -nodes -out ca.cert -subj "/C=US/ST
 openssl req -newkey rsa:2048 -nodes -out consul.csr -keyout consul.key -subj "/C=US/ST=New York/L=New York/O=NYC Department of Records and Information Services/OU=IT/CN=consul"
 openssl ca -batch -config myca.conf -notext -in consul.csr -out consul.cert
 cp ca.cert consul.key consul.cert /etc/consul.d/ssl
-# ln -s /vagrant/build_scripts/consul_setup/encrypt.json /etc/consul.d/bootstrap/encrypt.json
 
 # 4. Start Consul server with this command after build scripts are finished running
 # consul agent -config-dir /etc/consul.d/bootstrap
