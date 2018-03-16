4b132feda7996e5f1699a388113fce205f234ab7
Gary Zhou <gzhou@records.nyc.gov>
2018-03-16 10:55:53 -0400

    Consolidate duplicate code into function

---
+++ app/agency/api/utils.py
@@ -0,0 +1,15 @@
+from app.models import Agencies
+
+
+def get_active_users_as_choices(agency_ein):
+    """
+
+    :param agency_ein:
+    :return:
+    """
+    active_users = sorted(
+        [(user.guid, user.name)
+         for user in Agencies.query.filter_by(ein=agency_ein).one().active_users],
+        key=lambda x: x[1])
+    active_users.insert(0, ('', 'All'))
+    return active_users
--- app/agency/api/views.py
+++ app/agency/api/views.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_login import current_user, login_required
 
 from app.agency.api import agency_api_blueprint
-from app.models import Agencies
+from app.agency.api.utils import get_active_users_as_choices
 
 
 @agency_api_blueprint.route('/active_users/<string:agency_ein>', methods=['GET'])
@@ -17,12 +17,7 @@ def get_active_users(agency_ein):
                           "is_admin": True}), 200
     """
     if current_user.is_agency_admin(agency_ein):
-        active_users = sorted(
-            [(user.guid, user.name)
-             for user in Agencies.query.filter_by(ein=agency_ein).one().active_users],
-            key=lambda x: x[1])
-        active_users.insert(0, ('', 'All'))
-        return jsonify({"active_users": active_users, "is_admin": True}), 200
+        return jsonify({"active_users": get_active_users_as_choices(agency_ein), "is_admin": True}), 200
 
     elif current_user.is_agency_active(agency_ein):
         active_users = [
