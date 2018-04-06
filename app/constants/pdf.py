638371d22b983c20e644e7c08650f57b24c73d6b
Joel Castillo <jocastillo@records.nyc.gov>
2018-04-06 15:34:55 -0400

    Working backend implementation of PDF from LaTeX

---
+++ app/constants/pdf.py
@@ -0,0 +1,12 @@
+LATEX_TEMPLATE_CONFIG = {
+    'block_start_string': '\BLOCK{',
+    'block_end_string': '}',
+    'variable_start_string': '\VAR{',
+    'variable_end_string': '}',
+    'comment_start_string': '\#{',
+    'comment_end_string': '}',
+    'line_statement_prefix': '%%',
+    'line_comment_prefix': '%#',
+    'trim_blocks': True,
+    'autoescape': False
+}
