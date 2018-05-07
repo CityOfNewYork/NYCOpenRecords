75da893ed6529877868fba1504adf305a5fc0ec5
Joel Castillo <jocastillo@records.nyc.gov>
2018-05-07 17:36:38 -0400

    Fix quoting and layout

--- app/static/js/request/new-request-user.js
+++ app/static/js/request/new-request-user.js
@@ -5,9 +5,10 @@
 "use strict";
 
 $(document).ready(function () {
-
-    $("input[name='tz-name']").val(jstz.determine().name());
-
+    $(window).load(function () {
+        // Determine if the agencyRequestInstructions need to be shown on page load.
+        getRequestAgencyInstructions();
+    });
 
     function getRequestAgencyInstructions() {
         /*
@@ -37,6 +38,8 @@ $(document).ready(function () {
         });
     }
 
+    $("input[name='tz-name']").val(jstz.determine().name());
+    
     $("#request-category").change(function () {
         $.ajax({
             url: "/request/agencies",
@@ -53,18 +56,16 @@ $(document).ready(function () {
                     opt.value = data[i][0];
                     sel.append(opt);
                 }
+                // Determine if the agencyRequestInstructions need to be shown on page load.
+                getRequestAgencyInstructions();
             }
         });
-        // Determine if the agencyRequestInstructions need to be shown on page load.
-        getRequestAgencyInstructions();
     });
 
-    // ajax call to get additional information for the specified agency
+
     $("#request-agency").change(function () {
-        // Determine if the agencyRequestInstructions need to be shown on page load.
         getRequestAgencyInstructions();
     });
-
 
     $("#request-agency-instructions-toggle").click(function () {
         var el = $("#request-agency-instructions-toggle");
@@ -79,48 +80,48 @@ $(document).ready(function () {
             requestInstructionsContentDiv.hide();
         }
     });
-// javascript to add tooltip popovers when selecting the title and description
-    $('#request-title').attr({
-        'data-placement': "top",
-        'data-trigger': "hover focus",
-        'data-toggle': "popover",
-        'data-content': "Public Advocate Emails from 2015",
+    // javascript to add tooltip popovers when selecting the title and description
+    $("#request-title").attr({
+        "data-placement": "top",
+        "data-trigger": "hover focus",
+        "data-toggle": "popover",
+        "data-content": "Public Advocate Emails from 2015",
         title: "Example Title"
     });
-    $('#request-title').popover();
-// $('#request-title').click(function(){
-//     $('#request-title').popover('show');
-// });
+    $("#request-title").popover();
+    // $("#request-title").click(function(){
+    //     $("#request-title").popover("show");
+    // });
 
-    $('#request-description').attr({
-        'data-placement': "top",
-        'data-trigger': "hover focus",
-        'data-toggle': "popover",
-        'data-content': "Topic: Public Advocate Emails from 2015. Emails that mention bike lanes or bicycle lanes from the Public Advocate's Office between July 27, 2015 and September 10, 2015.",
+    $("#request-description").attr({
+        "data-placement": "top",
+        "data-trigger": "hover focus",
+        "data-toggle": "popover",
+        "data-content": "Topic: Public Advocate Emails from 2015. Emails that mention bike lanes or bicycle lanes from the Public Advocate's Office between July 27, 2015 and September 10, 2015.",
         title: "Example Request"
     });
-    $('#request-description').click(function () {
-        $('#request-description').popover('show');
+    $("#request-description").click(function () {
+        $("#request-description").popover("show");
     });
-    $('#request-description').popover();
-// $('#request-description').click(function(){
-//     $('#request-description').popover('show');
-// });
+    $("#request-description").popover();
+    // $("#request-description").click(function(){
+    //     $("#request-description").popover("show");
+    // });
 
-// Apply parsley validation styles to the input forms for a new request.
-    $('#request-title').attr('data-parsley-required', '');
-    $('#request-title').attr('data-parsley-maxlength', 90);
-    $('#request-agency').attr('data-parsley-required', '');
-    $('#request-description').attr('data-parsley-required', '');
-    $('#request-description').attr('data-parsley-maxlength', 5000);
+    // Apply parsley validation styles to the input forms for a new request.
+    $("#request-title").attr("data-parsley-required", "");
+    $("#request-title").attr("data-parsley-maxlength", 90);
+    $("#request-agency").attr("data-parsley-required", "");
+    $("#request-description").attr("data-parsley-required", "");
+    $("#request-description").attr("data-parsley-maxlength", 5000);
 
-// Limit the size of the file upload to 20 Mb. Second parameter is number of Mb's.
-    $('#request-file').attr('data-parsley-max-file-size', "20");
+    // Limit the size of the file upload to 20 Mb. Second parameter is number of Mb's.
+    $("#request-file").attr("data-parsley-max-file-size", "20");
 
-// Specify container for file input parsley error message
-    $('#request-file').attr("data-parsley-errors-container", ".file-error");
+    // Specify container for file input parsley error message
+    $("#request-file").attr("data-parsley-errors-container", ".file-error");
 
-// Set name of the file to the text of filename div if file exists
+    // Set name of the file to the text of filename div if file exists
     $("#request-file").change(function () {
         var file = this.files[0];
         var isChrome = window.chrome;
@@ -137,7 +138,7 @@ $(document).ready(function () {
         }
     });
 
-// Clear the file from input and the name from filename div
+    // Clear the file from input and the name from filename div
     $("#clear-file").click(function () {
         if ($(".file-error").is(":visible")) {
             $(".file-error").hide();
@@ -146,7 +147,7 @@ $(document).ready(function () {
         $("#filename").text("");
     });
 
-    $('#request-form').parsley().on('form:validate', function () {
+    $("#request-form").parsley().on("form:validate", function () {
         // Do stuff when parsley validates
         // TODO: this or combine (see the other new-request-* js files)
         if ($("#request-file").parsley().isValid() === false) {
@@ -157,33 +158,32 @@ $(document).ready(function () {
         }
     });
 
-// Clear error messages for form.request_file on submit ...
-    $('#submit').click(function () {
-        $('.upload-error').remove();
+    // Clear error messages for form.request_file on submit .
+    $("#submit").click(function () {
+        $(".upload-error").remove();
     });
-// ... or on input change for request_file
-    $('#request-file').change(function () {
-        $('.upload-error').remove();
+    // . or on input change for request_file
+    $("#request-file").change(function () {
+        $(".upload-error").remove();
     });
 
-// Disable submit button on form submission
-    $('#request-form').submit(function () {
+    // Disable submit button on form submission
+    $("#request-form").submit(function () {
         // Prevent multiple submissions
         $(this).submit(function () {
             return false;
         });
-        $('#submit').hide();
-        $('#processing-submission').show()
+        $("#submit").hide();
+        $("#processing-submission").show()
     });
 
-// Character count for creating a new request
-    $('#request-title').keyup(function () {
+    // Character count for creating a new request
+    $("#request-title").keyup(function () {
         characterCounter("#title-character-count", 90, $(this).val().length)
     });
 
-    $('#request-description').keyup(function () {
+    $("#request-description").keyup(function () {
         characterCounter("#description-character-count", 5000, $(this).val().length)
     });
 
-})
-;
+});
