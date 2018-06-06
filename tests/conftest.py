efdac8ca427bd66a959e98e8a43b73faec9e6030
Joel Castillo <jocastillo@records.nyc.gov>
2018-06-06 10:45:31 -0400

    Working tests for main

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,64 +14,48 @@ from sqlalchemy.orm import sessionmaker
 from alembic.command import upgrade as alembic_upgrade
 from alembic.config import Config as AlembicConfig
 
-from app import create_app
+from app import create_app, db as _db
 from config import config as base_config
 
 
-def pytest_configure(config):
-    sys._called_from_test = True
+@pytest.yield_fixture(scope='session')
+def app():
+    _app = create_app(config_name='testing')
+    ctx = _app.app_context()
+    ctx.push()
 
+    yield _app
 
-def pytest_unconfigure(config):
-    del sys._called_from_test
-
-
-@pytest.fixture(scope='session')
-def flask_app(request):
-    app = create_app('testing')
-    print('\n----- CREATE FLASK APPLICATION\n')
-
-    context = app.app_context()
-    context.push()
-    yield app
-    print('\n----- CREATE FLASK APPLICATION CONTEXT\n')
-
-    context.pop()
-    print('\n----- RELEASE FLASK APPLICATION CONTEXT\n')
+    ctx.pop()
 
 
 @pytest.fixture(scope='session')
-def client(request, flask_app):
-    print('\n----- CREATE FLASK TEST CLIENT\n')
-    return flask_app.test_client()
+def testapp(app):
+    return app.test_client()
 
 
-@pytest.fixture(scope='session')
-def db(request):
-    config = base_config['testing']
-    engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo=True)
-    session_factory = sessionmaker(bind=engine)
-    print('\n----- CREATE TEST DB CONNECTION POOL\n')
-
-    _db = {
-        'engine': engine,
-        'session_factory': session_factory,
-    }
+@pytest.yield_fixture(scope='session')
+def db(app):
+    _db.app = app
     _db.create_all()
-    print('\n----- RUN ALEMBIC MIGRATION\n')
+
     yield _db
-    print('\n----- CREATE TEST DB INSTANCE POOL\n')
 
-    engine.dispose()
-    print('\n----- RELEASE TEST DB CONNECTION POOL\n')
+    _db.drop_all()
 
 
-@pytest.fixture(scope='function')
-def session(request, db):
-    session = db['session_factory']()
-    yield session
-    print('\n----- CREATE DB SESSION\n')
+@pytest.fixture(scope='function', autouse=True)
+def session(db):
+    connection = db.engine.connect()
+    transaction = connection.begin()
 
-    session.rollback()
-    session.close()
-    print('\n----- ROLLBACK DB SESSION\n')
+    options = dict(bind=connection, binds={})
+    session_ = db.create_scoped_session(options=options)
+
+    db.session = session_
+
+    yield session_
+
+    transaction.rollback()
+    connection.close()
+    session_.remove()
--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -10,6 +10,7 @@ This module contains the tests for the OpenRecords `/` endpoint.
 import pytest
 
 from flask import Flask
+from app.models import Emails
 
 
 def test_index(client: Flask.test_client):
@@ -18,8 +19,9 @@ def test_index(client: Flask.test_client):
     Args:
         client (Flask.test_client): The test client used to access the endpoint
     """
-    response = client.get('/')
-    assert b"""<div class="col-sm-12 request-record-button">
+    response = client.get("/")
+    assert (
+        b"""<div class="col-sm-12 request-record-button">
             <div class="col-sm-6">
                 <a href="/request/view_all">
                     <button class="btn btn-primary btn-lg btn-block request-record-button-size">Check for Similar Requests</button>
@@ -30,7 +32,9 @@ def test_index(client: Flask.test_client):
                     <button class="btn btn-primary btn-lg btn-block request-record-button-size">Request a Record</button>
                 </a>
             </div>
-        </div>""" in response.data
+        </div>"""
+        in response.data
+    )
 
 
 def test_status(client: Flask.test_client):
@@ -40,33 +44,90 @@ def test_status(client: Flask.test_client):
         client (Flask.test_client): The test client used to access the endpoint
     """
     # Test `/index.html`
-    response = client.get('/index.html')
+    response = client.get("/index.html")
     assert response.status_code == 200
 
-    response = client.post('/index.html')
+    response = client.post("/index.html")
     assert response.status_code == 405
 
     # Test `/status`
-    response = client.get('/status')
+    response = client.get("/status")
     assert response.status_code == 200
-    response = client.post('/status')
+    response = client.post("/status")
     assert response.status_code == 405
 
 
 @pytest.mark.parametrize(
-    ('name', 'email', 'subject', 'message', 'error'),
-    (('John Doris', 'john.doris@records.nyc.gov', 'Test Subject', 'Test Message',
-      b'Your message has been sent. We will get back to you.'),
-     ('', 'john.doris@records.nyc.gov', 'Test Subject', 'Test Message', b'Cannot send email.'),
-     ('John Doris', '', 'Test Subject', 'Test Message', b'Cannot send email.'),
-     ('John Doris', 'john.doris@records.nyc.gov', '', 'Test Message', b'Cannot send email.'),
-     ('John Doris', 'john.doris@records.nyc.gov', 'Test Subject', '',
-      b'Cannot send email'),
-     ('', '', '', '', b'Cannot send email.')
-     )
+    ("name", "email", "subject", "message", "app_response"),
+    (
+        (
+            "John Doris",
+            "john.doris@records.nyc.gov",
+            "Test Subject",
+            "Test Message",
+            b"Your message has been sent. We will get back to you.",
+        ),
+        ("", "john.doris@records.nyc.gov", "Test Subject",
+         "Test Message", b"Cannot send email."),
+        ("John Doris", "", "Test Subject", "Test Message", b"Cannot send email."),
+        ("John Doris", "john.doris@records.nyc.gov",
+         "", "Test Message", b"Cannot send email."),
+        ("John Doris", "john.doris@records.nyc.gov",
+         "Test Subject", "", b"Cannot send email"),
+        ("", "", "", "", b"Cannot send email."),
+    ),
 )
-def test_technical_support_page(client: Flask.test_client, db, name: str, email: str, subject: str, message: bytes,
-                                error: str):
+def test_contact_page(
+    client: Flask.test_client, db, name: str, email: str, subject: str, message: bytes, app_response: str
+):
+    """Test the contact endpoint / form for OpenRecords
+
+    Args:
+        client (Flask.test_client): Test client used to access the technical-support page
+        name (str): Name of the person submitting the form (required)
+        email (str): Email of the person submitting the form (required)
+        subject (str): Subject of the message being sent out
+        message (str): Message being sent out
+        app_response (str): Message displayed after post
+    """
+    data = {"name": name, "email": email, "subject": subject,
+            "message": message, "app_response": app_response}
+
+    # Test `/contact`/
+    response = client.get("/contact")
+    assert response.status_code == 200
+    assert b'<h2 class="text-center">Technical Support</h2>' in response.data
+    assert b"Use this form only for technical support." in response.data
+    response = client.post("/contact", data=data)
+    assert response.status_code == 200
+    assert app_response in response.data
+    email_message = Emails.query.all()
+    assert len(email_message) == 1
+
+
+@pytest.mark.parametrize(
+    ("name", "email", "subject", "message", "app_response"),
+    (
+        (
+            "John Doris",
+            "john.doris@records.nyc.gov",
+            "Test Subject",
+            "Test Message",
+            b"Your message has been sent. We will get back to you.",
+        ),
+        ("", "john.doris@records.nyc.gov", "Test Subject",
+         "Test Message", b"Cannot send email."),
+        ("John Doris", "", "Test Subject", "Test Message", b"Cannot send email."),
+        ("John Doris", "john.doris@records.nyc.gov",
+         "", "Test Message", b"Cannot send email."),
+        ("John Doris", "john.doris@records.nyc.gov",
+         "Test Subject", "", b"Cannot send email"),
+        ("", "", "", "", b"Cannot send email."),
+    ),
+)
+def test_technical_support_page(
+    client: Flask.test_client, db, name: str, email: str, subject: str, message: bytes, app_response: str
+):
     """Test the technical support endpoint / form for OpenRecords
 
     Args:
@@ -75,32 +136,17 @@ def test_technical_support_page(client: Flask.test_client, db, name: str, email:
         email (str): Email of the person submitting the form (required)
         subject (str): Subject of the message being sent out
         message (str): Message being sent out
-        error (str): Message displayed after post
+        app_response (str): Message displayed after post
     """
-    data = {
-        'name': name,
-        'email': email,
-        'subject': subject,
-        'message': message,
-        'error': error
-    }
-
-    # Test `/contact`/
-    response = client.get('/contact')
+    data = {"name": name, "email": email, "subject": subject,
+            "message": message, "app_response": app_response}
+    # Test `/technical-support`
+    response = client.get("/technical-support")
     assert response.status_code == 200
     assert b'<h2 class="text-center">Technical Support</h2>' in response.data
-    assert b'Use this form only for technical support.' in response.data
-    pytest.set_trace()
-    response = client.post('/contact', data=data)
+    assert b"Use this form only for technical support." in response.data
+    response = client.post("/technical-support", data=data)
     assert response.status_code == 200
-    assert error in response.data
-
-
-    # # Test `/technical-support`
-    # response = client.get('/technical-support')
-    # assert response.status_code == 200
-    # assert b'<h2 class="text-center">Technical Support</h2>' in response.data
-    # assert b'<p align="center">Use this form only for technical support. Agency contact for other questions and information is available within the actual request.</p>' in response.data
-    # response = client.post('/technical-support', data=data)
-    # assert response.status_code == 200
-    # assert message in response.data
+    assert app_response in response.data
+    email_message = Emails.query.all()
+    assert len(email_message) == 1
