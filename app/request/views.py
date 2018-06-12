dab639e63dd369716fb02f222d8973c758798097
Joel Castillo <jocastillo@records.nyc.gov>
2018-06-12 15:26:46 -0400

    Update tz-name usages throughout codebase to default to "APP_TIMEZONE"

--- app/request/views.py
+++ app/request/views.py
@@ -112,7 +112,7 @@ def new():
                 return render_template(new_request_template, form=form, site_key=site_key)
 
         custom_metadata = json.loads(flask_request.form['custom-request-forms-data'])
-        tz_name = flask_request.form['tz-name'] if flask_request.form['tz-name'] else 'America/New_York'
+        tz_name = flask_request.form['tz-name'] if flask_request.form['tz-name'] else current_app.config['APP_TIMEZONE']
         if current_user.is_public:
             request_id = create_request(form.request_title.data,
                                         form.request_description.data,
