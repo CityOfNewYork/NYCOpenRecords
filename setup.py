0e5ab10454e776adbea4fb31a01d744cfa9cf1c3
Joel Castillo <jocastillo@records.nyc.gov>
2018-06-19 14:25:27 -0400

    Remove unneeded files for this PR

--- setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-setup(
-    name='app',
-    version="1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=["flask","flask_login","datetime","itertools","pytz","jsonschema","flask_wtf"],
-)
