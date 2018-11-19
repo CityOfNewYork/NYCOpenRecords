b1f39b275a058aa48c5cc94c7014e08c833494c8
Jonathan Yu <jyu@records.nyc.gov>
2018-11-19 18:28:53 -0500

    Started adding keyboard accessibility for upload file

--- app/static/js/request/new-request-agency.js
+++ app/static/js/request/new-request-agency.js
@@ -205,7 +205,24 @@ $(document).ready(function () {
             $(".file-error").hide();
         }
         $("#request-file").val("");
-        $("#filename").text("");
+        $("#filename").val("");
+        $("#filename").attr('placeholder', 'No file uploaded');
+    });
+
+    // trigger file explorer on space and enter
+    $('#buttonlabel span[role=button]').bind('keypress keyup', function (e) {
+        if (e.which === 32 || e.which === 13) {
+            e.preventDefault();
+            $('#request-file').click();
+        }
+    });
+
+    // return chosen filename to additional input
+    $('#request-file').change(function (e) {
+        var filename = $('#request-file').val().split('\\').pop();
+        $('#filename').val(filename);
+        $('#filename').attr('placeholder', filename);
+        $('#filename').focus();
     });
 
     // Contact information validation
