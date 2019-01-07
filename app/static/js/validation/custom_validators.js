d7bc02e85c713f8c012757b8cab50ebb9b912b2b
Jonathan Yu <jyu@records.nyc.gov>
2019-01-07 10:31:46 -0500

    Single quotes to double quotes

--- app/static/js/validation/custom_validators.js
+++ app/static/js/validation/custom_validators.js
@@ -4,14 +4,14 @@
 
 "use strict";
 
-window.Parsley.addValidator('maxFileSize', {
+window.Parsley.addValidator("maxFileSize", {
   validateString: function(_value, maxSize, parsleyInstance) {
     var files = parsleyInstance.$element[0].files;
     return files.length != 1  || files[0].size <= maxSize * 1000000;
   },
-  requirementType: 'integer',
+  requirementType: "integer",
   messages: {
-    en: '<span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;' +
-    '<strong>The file cannot be larger than %s Mb.</strong> Please choose a smaller file.'
+    en: "<span class=\"glyphicon glyphicon-exclamation-sign\"></span>&nbsp;" +
+    "<strong>The file cannot be larger than %s Mb.</strong> Please choose a smaller file."
   }
 });
