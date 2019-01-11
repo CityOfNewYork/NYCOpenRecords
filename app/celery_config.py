b8bdf0472b463d08c3fe23775ca2064cea43bb54
Jonathan Yu <jyu@records.nyc.gov>
2019-01-11 17:58:33 -0500

    Updated comments

--- app/celery_config.py
+++ app/celery_config.py
@@ -8,7 +8,7 @@ CELERY_TIMEZONE = 'EST'
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
 
 CELERYBEAT_SCHEDULE = {
-    # Every weekday at 3AM EST
+    # Every weekday at 7AM EST
     'update_request_statuses': {
         'task': 'app.jobs.update_request_statuses',
         'schedule': crontab(minute='0', hour='7', day_of_week='1-5')
