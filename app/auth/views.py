afdad9fadd0788b12018061e16092ffcb566a211
Joel Castillo <jocastillo@records.nyc.gov>
2019-01-16 11:54:14 -0500

    Adding documentation

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,22 +13,22 @@ from flask import (
     render_template,
     request,
     session,
-    url_for
+    url_for,
 )
 from flask_login import (
     current_app,
     current_user,
     login_required,
     login_user,
-    logout_user
+    logout_user,
 )
 from app import csrf
 from app.auth import auth
 from app.auth.constants.error_msg import UNSAFE_NEXT_URL
 from app.auth.forms import (
-    LDAPLoginForm,
+    BasicLoginForm,
     ManageAgencyUserAccountForm,
-    ManageUserAccountForm
+    ManageUserAccountForm,
 )
 from app.auth.utils import (
     find_user_by_email,
@@ -39,11 +39,128 @@ from app.auth.utils import (
     saml_acs,
     saml_slo,
     saml_sls,
-    update_openrecords_user
+    update_openrecords_user,
 )
 
 
-@auth.route('/saml', methods=['GET', 'POST'])
+@auth.route("/login", methods=["GET"])
+def login():
+    """Handle login redirects for users. 
+
+    This application supports three methods for login: SAML 2.0, LDAP, and Local Authentication
+
+    SAML 2.0 integrates with the City of New York Authentication System (NYC.ID). Users will be redirected to the SAML
+    authentication endpoint. Please see app.auth.saml for details.
+
+    LDAP authentication will redirect the user to a Flask login form and authenticate them using the LDAP protocol.
+    Please see app.auth.ldap_login for details.
+
+    Local Auth is used for development and testing purposes only. It allows a user to login using any password, as long
+    as the email is valid and in the database.
+
+    The three methods are called in the preferred method for authentication: 1) SAML 2) LDAP 3) Local Auth
+
+    Based off of: https://flask-login.readthedocs.io/en/latest/#login-example
+
+    Args:
+        next (str): URL to send the user to after successful authentication.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Response redirecting the browser to the proper URL for login
+    """
+    next_url = request.args.get("next")
+
+    if current_app.config["USE_SAML"]:
+        if next_url:
+            return redirect(url_for("auth.saml", sso2=next_url))
+
+    elif current_app.config["USE_LDAP"]:
+        return redirect(url_for("auth.ldap_login", next=next_url))
+
+    elif current_app.config["USE_LOCAL_AUTH"]:
+
+    return abort(404)
+
+
+@auth.route("/logout", methods=["GET"])
+def logout():
+    """
+    Unified logout endpoint for all authentication types.
+
+    GET Args:
+        timeout (bool): If True, logout is being called due to a session timeout.
+        forced_logout (bool): If True, logout is being called due to a duplicate session.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirect to the appropriate logout endpoint.
+    """
+    timeout = request.args.get("timeout", False)
+    forced_logout = request.args.get("forced_logout", False)
+
+    if current_app.config["USE_LDAP"]:
+        return redirect(
+            url_for("auth.ldap_logout", timeout=timeout, forced_logout=forced_logout)
+        )
+
+    elif current_app.config["USE_OAUTH"]:
+        return redirect(
+            url_for("auth.oauth_logout", timeout=timeout, forced_logout=forced_logout)
+        )
+
+    elif current_app.config["USE_SAML"]:
+        return redirect(
+            url_for(
+                "auth.saml", slo="true", timeout=timeout, forced_logout=forced_logout
+            )
+        )
+
+    elif current_app.config["USE_LOCAL_AUTH"]:
+        logout_user()
+        session.destroy()
+        if timeout:
+            flash("Your session timed out. Please login again", category="info")
+        return redirect(url_for("main.index"))
+
+    return abort(404)
+
+
+@auth.route("/manage", methods=["GET", "POST"])
+@login_required
+def manage():
+    """
+    Allow users to manage their OpenRecords specific attributes.
+
+    For POST requests, updates the users data in the database.
+    For GET requests, pulls the current user data from the database and pre-populates the form.
+
+    Requires users to have an active authentication session.
+
+    Returns:
+        Flask Response with Manage Page
+
+    """
+    if current_user.is_agency:
+        form = ManageAgencyUserAccountForm(user=current_user)
+    else:
+        form = ManageUserAccountForm(user=current_user)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            update_openrecords_user(form)
+            redirect(url_for("auth.manage"))
+        else:
+            flash("Account cannot be updated.", category="danger")
+            return render_template("auth/manage_account.html", form=form)
+    else:
+        form.autofill()
+
+    return render_template(
+        "auth/manage_account.html", form=form, is_agency=current_user.is_agency
+    )
+
+
+# SAML Authentication Endpoints
+@auth.route("/saml", methods=["GET", "POST"])
 @csrf.exempt
 def saml():
     """
@@ -56,7 +173,13 @@ def saml():
         slo - Generate a Logout request for the IdP
         sls - Handle a Logout Response from the IdP and destroy the local session
 
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirects the user to the proper location in the SAML Auth Workflow.
+
     """
+    if not current_app.config['USE_SAML']:
+        return redirect(url_for('auth.login'))
+
     # 1. Convert Flask Request object to OneLogin Request Dictionary
     onelogin_request = prepare_onelogin_request(request)
 
@@ -64,124 +187,35 @@ def saml():
     onelogin_saml_auth = init_saml_auth(onelogin_request)
 
     # 3. Handle request based on GET parameter.
-    if 'sso' in request.args or len(request.args) == 0:
+    if "sso" in request.args or len(request.args) == 0:
         return redirect(onelogin_saml_auth.login())
-    elif 'sso2' in request.args:
-        return_to = '{host_url}{endpoint}'.format(host_url=request.host_url, endpoint=request.args.get('sso2', '/')[1:])
+    elif "sso2" in request.args:
+        return_to = "{host_url}{endpoint}".format(
+            host_url=request.host_url, endpoint=request.args.get("sso2", "/")[1:]
+        )
         return redirect(onelogin_saml_auth.login(return_to))
-    elif 'acs' in request.args:
+    elif "acs" in request.args:
         return saml_acs(onelogin_saml_auth, onelogin_request)
-    elif 'slo' in request.args:
+    elif "slo" in request.args:
         return redirect(saml_slo(onelogin_saml_auth))
-    elif 'sls' in request.args:
+    elif "sls" in request.args:
         return saml_sls(onelogin_saml_auth)
     else:
-        flash('Oops! Something went wrong. Please try to perform your action again later.', category="warning")
-        return redirect(url_for('main.index'))
+        flash(
+            "Oops! Something went wrong. Please try to perform your action again later.",
+            category="warning",
+        )
+        return redirect(url_for("main.index"))
 
 
-@auth.route('/login', methods=['GET'])
-def login():
+@auth.route("/metadata/", methods=["GET"])
+def metadata():
     """
-    Based off of: https://flask-login.readthedocs.io/en/latest/#login-example
-    If using LDAP, see ldap_login().
-    If using SAML/OAuth, check for the presence of an access token
-    in the session, which is used to fetch user information for processing.
-    If no token exists, send the user to the authorization url
-    (first leg of the OAuth 2 workflow).
-    * NOTE *
-    Since we are using OAuth Mobile Application Flow to fetch the information
-    normally retrieved in a SAML Assertion, the url resulting from authorization
-    is in the format 'redirect_uri#access_token=123guid=ABC...'. Notice the fragment
-    identifier ('#') in place of what would normally be the '?' separator.
-    Since Flask drops everything after the identifier, we must extract these values
-    client-side in order to forward them to the server. Therefore, the redirect uri
-    we are using is our home page (main.index, along with the 'next' url if present)
-    which contains a script that detects the presence of an access token
-    and redirects to the intended OAuth callback (auth.authorize).
-    https://tools.ietf.org/html/rfc3986#section-3.5
-    """
-    next_url = request.args.get('next')
-
-    if current_app.config['USE_SAML']:
-        if next_url:
-            return redirect(url_for('auth.saml', sso2=next_url))
-
-    elif current_app.config['USE_LDAP']:
-        return redirect(url_for('auth.ldap_login', next=next_url))
-
-    elif current_app.config['USE_LOCAL_AUTH']:
-        login_form = LDAPLoginForm()
-        if request.method == 'POST':
-            email = request.form['email']
-
-            user = find_user_by_email(email)
-
-            if user is not None:
-                authenticated = True
-
-                if authenticated:
-                    login_user(user)
-                    session.regenerate()
-                    session['user_id'] = current_user.get_id()
-
-                    next_url = request.form.get('next')
-                    if not is_safe_url(next_url):
-                        return abort(400, UNSAFE_NEXT_URL)
-
-                    return redirect(next_url or url_for('main.index'))
-
-                flash("Invalid username/password combination.", category="danger")
-                return render_template('auth/local_login_form.html', login_form=login_form)
-            else:
-                flash("User not found. Please contact your agency FOIL Officer to gain access to the system.",
-                      category="warning")
-                return render_template('auth/local_login_form.html', login_form=login_form)
-
-        elif request.method == 'GET':
-            return render_template(
-                'auth/local_login_form.html',
-                login_form=login_form,
-                next_url=request.args.get('next', ''))
-
-    return abort(404)
-
-
-@auth.route('/logout', methods=['GET'])
-def logout():
-    """Unified logout endpoint for all authentication types.
-
-    GET Args:
-        timeout (bool): If True, logout is being called due to a session timeout.
-        forced_logout (bool): If True, logout is being called due to a duplicate session.
+    Access the SAML SP metadata for this application
 
     Returns:
-        Redirect to the appropriate logout endpoint.
+        HTTP Response (werkzeug.wrappers.Response): XML SP Metadata
     """
-    timeout = request.args.get('timeout', False)
-    forced_logout = request.args.get('forced_logout', False)
-
-    if current_app.config['USE_LDAP']:
-        return redirect(url_for('auth.ldap_logout', timeout=timeout, forced_logout=forced_logout))
-
-    elif current_app.config['USE_OAUTH']:
-        return redirect(url_for('auth.oauth_logout', timeout=timeout, forced_logout=forced_logout))
-
-    elif current_app.config['USE_SAML']:
-        return redirect(url_for('auth.saml', slo='true', timeout=timeout, forced_logout=forced_logout))
-
-    elif current_app.config['USE_LOCAL_AUTH']:
-        logout_user()
-        session.destroy()
-        if timeout:
-            flash("Your session timed out. Please login again", category='info')
-        return redirect(url_for('main.index'))
-
-    return abort(404)
-
-
-@auth.route('/metadata/', methods=['GET'])
-def metadata():
     req = prepare_onelogin_request(request)
     saml_auth = init_saml_auth(req)
     settings = saml_auth.get_settings()
@@ -190,43 +224,32 @@ def metadata():
 
     if len(errors) == 0:
         resp = make_response(metadata, 200)
-        resp.headers['Content-Type'] = 'text/xml'
+        resp.headers["Content-Type"] = "text/xml"
     else:
-        resp = make_response(', '.join(errors), 500)
+        resp = make_response(", ".join(errors), 500)
     return resp
 
 
-@auth.route('/manage', methods=['GET', 'POST'])
-@login_required
-def manage():
-    if current_user.is_agency:
-        form = ManageAgencyUserAccountForm(user=current_user)
-    else:
-        form = ManageUserAccountForm(user=current_user)
-
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            update_openrecords_user(form)
-            redirect(url_for('auth.manage'))
-        else:
-            flash("Account cannot be updated.", category="danger")
-            return render_template('auth/manage_account.html', form=form)
-    else:
-        form.autofill()
-
-    return render_template('auth/manage_account.html', form=form, is_agency=current_user.is_agency)
-
-
-# LDAP -----------------------------------------------------------------------------------------------------------------
-
-@auth.route('/ldap_login', methods=['GET', 'POST'])
+# LDAP Authentication Endpoints
+@auth.route("/ldap_login", methods=["GET", "POST"])
 def ldap_login():
-    if not current_app.config['USE_LDAP']:
-        return redirect(url_for('auth.login'))
-    login_form = LDAPLoginForm()
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    """
+    Login a user using the LDAP protocol
+
+    Args:
+        next (str): URL to redirect the user to if login is successful.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirects the user to the home page (if successful) or to the
+                                                    login page again (if unsuccessful)
+
+    """
+    if not current_app.config["USE_LDAP"]:
+        return redirect(url_for("auth.login"))
+    login_form = BasicLoginForm()
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
 
         user = find_user_by_email(email)
 
@@ -236,32 +259,110 @@ def ldap_login():
             if authenticated:
                 login_user(user)
                 session.regenerate()
-                session['user_id'] = current_user.get_id()
+                session["user_id"] = current_user.get_id()
 
-                next_url = request.form.get('next')
+                next_url = request.form.get("next")
                 if not is_safe_url(next_url):
                     return abort(400, UNSAFE_NEXT_URL)
 
-                return redirect(next_url or url_for('main.index'))
+                return redirect(next_url or url_for("main.index"))
 
             flash("Invalid username/password combination.", category="danger")
-            return render_template('auth/ldap_login_form.html', login_form=login_form)
+            return render_template("auth/ldap_login_form.html", login_form=login_form)
         else:
-            flash("User not found. Please contact your agency FOIL Officer to gain access to the system.",
-                  category="warning")
-            return render_template('auth/ldap_login_form.html', login_form=login_form)
+            flash(
+                "User not found. Please contact your agency FOIL Officer to gain access to the system.",
+                category="warning",
+            )
+            return render_template("auth/ldap_login_form.html", login_form=login_form)
 
-    elif request.method == 'GET':
+    elif request.method == "GET":
         return render_template(
-            'auth/ldap_login_form.html',
+            "auth/ldap_login_form.html",
             login_form=login_form,
-            next_url=request.args.get('next', ''))
+            next_url=request.args.get("next", ""),
+        )
 
 
-@auth.route('/ldap_logout', methods=['GET'])
-def ldap_logout(timed_out=False, forced_logout=False):
+@auth.route("/ldap_logout", methods=["GET"])
+def ldap_logout(timed_out=False):
+    """
+    Log a user out from the LDAP server.
+    Args:
+        timed_out (bool): If True, the users client side session timed-out.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirects the user to the home page
+
+    """
     logout_user()
     session.destroy()
     if timed_out:
-        flash("Your session timed out. Please login again", category='info')
-    return redirect(url_for('main.index'))
+        flash("Your session timed out. Please login again", category="info")
+    return redirect(url_for("main.index"))
+
+
+# Local Authentication Endpoints
+@auth.route('/local_login', methods=['GET', 'POST'])
+def local_auth():
+    """
+    Authenticate a user against the database (ignore password).
+
+    Allows developers to test functionality as valid users without needing to use a third party service.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirects the user to the home page (if successful) or to the
+                                                    login page again (if unsuccessful)
+    """
+    if not current_app.config["USE_LOCAL_AUTH"]:
+        return redirect(url_for('auth.login'))
+    login_form = BasicLoginForm()
+    if request.method == "POST":
+        email = request.form["email"]
+
+        user = find_user_by_email(email)
+
+        if user is not None:
+            authenticated = True
+
+            login_user(user)
+            session.regenerate()
+            session["user_id"] = current_user.get_id()
+
+            next_url = request.form.get("next")
+            if not is_safe_url(next_url):
+                return abort(400, UNSAFE_NEXT_URL)
+
+            return redirect(next_url or url_for("main.index"))
+        else:
+            flash(
+                "User not found. Please contact your agency FOIL Officer to gain access to the system.",
+                category="warning",
+            )
+            return render_template(
+                "auth/local_login_form.html", login_form=login_form
+            )
+
+    elif request.method == "GET":
+        return render_template(
+            "auth/local_login_form.html",
+            login_form=login_form,
+            next_url=request.args.get("next", ""),
+        )
+
+
+@auth.route("/local_logout", methods=["GET"])
+def local_logout(timed_out=False):
+    """
+    Log a user out from the server.
+    Args:
+        timed_out (bool): If True, the users client side session timed-out.
+
+    Returns:
+        HTTP Response (werkzeug.wrappers.Response): Redirects the user to the home page
+    """
+    logout_user()
+    session.destroy()
+    if timed_out:
+        flash("Your session timed out. Please login again", category="info")
+    return redirect(url_for("main.index"))
