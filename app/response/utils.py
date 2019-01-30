1115f1613a75567d12eed688a262d5c4bf9d5054
Joel Castillo <jocastillo@records.nyc.gov>
2019-01-30 14:39:25 -0500

    Remove unneeded syntax change

--- app/response/utils.py
+++ app/response/utils.py
@@ -1033,7 +1033,7 @@ def _acknowledgment_letter_handler(request_id, data):
         point_of_contact_user = assign_point_of_contact(acknowledgment.get('point_of_contact', None))
 
         template = render_template_string(contents.content,
-                                          days=acknowledgment.get('days', None),
+                                          days=acknowledgment['days'],
                                           response_due_date=acknowledgment['date'],
                                           date=request.date_submitted,
                                           user=point_of_contact_user)
