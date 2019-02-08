e476ad28f93c643e66965c41c5c036c810d19d52
Joel Castillo <jocastillo@records.nyc.gov>
2019-02-08 17:10:14 -0500

    Removes references to rhel.txt

--- build_scripts/app_setup/setup_virtualenv.sh
+++ build_scripts/app_setup/setup_virtualenv.sh
@@ -7,11 +7,6 @@ virtualenv --system-site-packages /home/vagrant/.virtualenvs/openrecords
 source /home/vagrant/.virtualenvs/openrecords/bin/activate
 pip install -r /vagrant/requirements/common.txt
 
-if [[ "$1" -eq rhel ]] || [[ "$2" -eq rhel ]]; then
-    pip install -r /vagrant/requirements/rhel.txt
-fi
-
-
 if [[ "$1" -eq development ]] || [[ "$2" -eq development ]]; then
     pip install -r /vagrant/requirements/dev.txt
 fi
