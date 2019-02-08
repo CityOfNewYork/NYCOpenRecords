8e5294e1b45a89a72884e12417118a7f52a87fff
Joel Castillo <jocastillo@records.nyc.gov>
2019-02-08 16:49:57 -0500

    Update app_setup.sh and setup_virtualenv.sh to avoid duplicate installs

--- build_scripts/app_setup/setup_virtualenv.sh
+++ build_scripts/app_setup/setup_virtualenv.sh
@@ -2,6 +2,16 @@
 
 source /opt/rh/rh-python35/enable
 
+mkdir /home/vagrant/.virtualenvs
 virtualenv --system-site-packages /home/vagrant/.virtualenvs/openrecords
 source /home/vagrant/.virtualenvs/openrecords/bin/activate
 pip install -r /vagrant/requirements/common.txt
+
+if [[ "$1" -eq rhel ]] || [[ "$2" -eq rhel ]]; then
+    pip install -r /vagrant/requirements/rhel.txt
+fi
+
+
+if [[ "$1" -eq development ]] || [[ "$2" -eq development ]]; then
+    pip install -r /vagrant/requirements/dev.txt
+fi
