9df955fa435d5a7de0930167e0ea09c666275cb0
Joel Castillo <jocastillo@records.nyc.gov>
2019-02-08 20:26:35 -0500

    Updates setuptools and pip in virtualenv

--- build_scripts/app_setup/setup_virtualenv.sh
+++ build_scripts/app_setup/setup_virtualenv.sh
@@ -5,6 +5,8 @@ source /opt/rh/rh-python35/enable
 mkdir /home/vagrant/.virtualenvs
 virtualenv --system-site-packages /home/vagrant/.virtualenvs/openrecords
 source /home/vagrant/.virtualenvs/openrecords/bin/activate
+pip install -U pip
+pip install -U setuptools
 pip install -r /vagrant/requirements/common.txt
 
 if [[ "$1" -eq development ]] || [[ "$2" -eq development ]]; then
