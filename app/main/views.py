fb4a5e291d211e23be29b299856bb949eebbe582
johnyu95 <jyu@records.nyc.gov>
2025-10-17 14:59:13 -0400

    Removed commented code

--- app/main/views.py
+++ app/main/views.py
@@ -25,35 +25,6 @@ from . import main
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    # import urllib.parse
-    # import os
-    #
-    # from azure.identity import DefaultAzureCredential
-    #
-    # # IMPORTANT! This code is for demonstration purposes only. It's not suitable for use in production.
-    # # For example, tokens issued by Microsoft Entra ID have a limited lifetime (24 hours by default).
-    # # In production code, you need to implement a token refresh policy.
-    #
-    # # Read URI parameters from the environment
-    # dbhost = os.environ['DBHOST']
-    # dbname = os.environ['DBNAME']
-    # dbuser = urllib.parse.quote(os.environ['DBUSER'])
-    # sslmode = os.environ['SSLMODE']
-    #
-    # # Use passwordless authentication via DefaultAzureCredential.
-    # # IMPORTANT! This code is for demonstration purposes only. DefaultAzureCredential() is invoked on every call.
-    # # In practice, it's better to persist the credential across calls and reuse it so you can take advantage of token
-    # # caching and minimize round trips to the identity provider. To learn more, see:
-    # # https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/identity/azure-identity/TOKEN_CACHING.md
-    # credential = DefaultAzureCredential()
-    #
-    # # Call get_token() to get a token from Microsft Entra ID and add it as the password in the URI.
-    # # Note the requested scope parameter in the call to get_token, "https://ossrdbms-aad.database.windows.net/.default".
-    # password = credential.get_token("https://ossrdbms-aad.database.windows.net/.default").token
-    #
-    # db_uri = f"postgresql://{dbuser}:{password}@{dbhost}/{dbname}?sslmode={sslmode}"
-    # print(db_uri)
-
     fresh_login = request.args.get('fresh_login', False)
     verify_mfa = request.args.get('verify_mfa', False)
     if current_user.is_authenticated:
